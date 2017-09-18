d9722c5dcdde5ff2cc674fc3311015a8492aebca
백기훈 <masinogns@gmail.com>
2017-09-18 10:04:29 +0900

    DP 가장 긴 증가하는 부분

--- src/기초알고리즘/다이나믹프로그래밍/가장긴증가하는부분.java
+++ src/기초알고리즘/다이나믹프로그래밍/가장긴증가하는부분.java
@@ -4,38 +4,28 @@ package 기초알고리즘.다이나믹프로그래밍;
  * Created by masinogns on 2017. 9. 14..
  *
  * D[N] = N일 때 가장 긴 증가하는 부분 수열의 길이
+ *
+ * D[i] = .... A[j], A[i]
+ * D[j] = .... A[j] ; A[j]를 마지막으로 하는 수열의 가장 큰 값 + 1
+ *
+ * D[i] = max(D[j]) + 1; j < i; A[j] < A[i]
+ *
+ *
  */
 public class 가장긴증가하는부분 {
-    public int solution(int N, int[] A) {
-
-        // 이렇게 하면 안된다. 왜냐하면 테스트 케이스 2번이 예외임
-        int[] D = new int[N+1];
-
-        D[1] = 1;
-
-        for (int n = 2; n <= N; n++){
-            if (A[n-2] < A[n-1])
-                D[n] = D[n-1] + 1;
-            else
-                D[n] = D[n-1];
-        }
-        return D[N];
-    }
-
 
     public int solution2(int N, int[] A) {
 
         int[] D = new int[N];
 
         for (int i = 0; i < N; i++){
-            D[i] = 1;
+            D[i] = 1;       // 이 부분에서 저장을 해줘서 밑에 if문에서 D[i] < D[k] +1부분이 가능하다
             for (int k = 0; k < i; k++){
                 // D[i] < D[k] + 1은 D[i]값이 비교하는 값보다 작을때만 값을 증가시키겠다는 의미 ,
                 // 크면 굳이 바꾸면 안되니까 넣어준 조건.
                 if (A[k] < A[i] && D[i] < D[k] + 1)
-                    D[i] = D[k ]+1;
+                    D[i] = D[k]+1;
             }
-
         }
 
         int ret = 0;
--- test/기초알고리즘/다이나믹프로그래밍/가장긴증가하는부분Test.java
+++ test/기초알고리즘/다이나믹프로그래밍/가장긴증가하는부분Test.java
@@ -11,13 +11,6 @@ public class 가장긴증가하는부분Test {
     가장긴증가하는부분 application = new 가장긴증가하는부분();
 
     @Test
-    public void name() throws Exception {
-        assertEquals(4, application.solution(6, new int[]{
-                10,20,10,30,20,50
-        }));
-    }
-
-    @Test
     public void nameB() throws Exception {
         assertEquals(4, application.solution2(6, new int[]{
                 10,20,10,30,20,50
@@ -26,12 +19,6 @@ public class 가장긴증가하는부분Test {
     @Test
     public void name2B() throws Exception {
         assertEquals(4, application.solution2(7, new int[]{
-                30,10,16,40,15,20,30
-        }));
-    }
-    @Test
-    public void name2() throws Exception {
-        assertEquals(4, application.solution(7, new int[]{
                 30,10,16,40,15,20,30
         }));
     }
