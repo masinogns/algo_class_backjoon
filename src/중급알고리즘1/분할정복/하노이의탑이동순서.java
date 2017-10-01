02ccaa1d5b86239144424af0121f3d905e915f97
백기훈 <masinogns@gmail.com>
2017-10-01 18:48:49 +0900

    분할정복 하노이 탑의 이동순서 ; 어려운 듯 쉬운 듯 어려운 분할 정복

--- src/중급알고리즘1/분할정복/하노이의탑이동순서.java
+++ src/중급알고리즘1/분할정복/하노이의탑이동순서.java
@@ -1,5 +1,7 @@
 package 중급알고리즘1.분할정복;
 
+import java.util.Scanner;
+
 /**
  * Created by masinogns on 2017. 9. 16..
  *
@@ -10,8 +12,14 @@ package 중급알고리즘1.분할정복;
  * 1. 1~n-1개 원판 x->z    solve(n-1, x, z)
  * 2. n번 원판 x->y        move(n, x, y )
  * 3. 1~n-1개 원판 z->y    solve(n-1, z, y)
+ *
+ * 1~2번째 원판을 1에서 2로 이동한다
+ * 3번째 원파을 1에서 3로 이동한다
+ * 1~2번째 원판을 2에서 3로 이동한다
  */
 public class 하노이의탑이동순서 {
+    public static StringBuilder sb;
+
     int count = 0;
 
     public int solution(int N) {
@@ -20,10 +28,28 @@ public class 하노이의탑이동순서 {
     }
 
     private void solve(int n, int x, int y) {
+        // 이동시킬 원판이 없으면 return으로 끝낸다
         if (n == 0) return;
         count++;
+        // x+y+z=6 --> z = 6-x-y
         solve(n-1, x, 6-x-y);
-        System.out.println(n+"번째 원판이 "+x+"에서 "+y+"로 이동했다");
+//        System.out.println(n+"번째 원판이 "+x+"에서 "+y+"로 이동했다");
+        sb.append(x+" "+y+"\n");
         solve(n-1, 6-x-y, y);
+
+    }
+
+    public static void main(String[] args) {
+         하노이의탑이동순서 application = new 하노이의탑이동순서();
+
+        Scanner scanner = new Scanner(System.in);
+        sb = new StringBuilder();
+
+        int N = scanner.nextInt();
+
+        int ret = application.solution(N);
+
+        System.out.println(ret);
+        System.out.println(sb);
     }
 }
