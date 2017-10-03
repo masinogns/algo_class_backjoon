7599d32149389748367d6bf34b6f2c80d7a27119
백기훈 <masinogns@gmail.com>
2017-10-03 16:48:59 +0900

    완전 탐색; 순열의 집합 백준꺼..

---
+++ src/중급알고리즘1/완전탐색0/순열의집합백준.java
@@ -0,0 +1,22 @@
+package 중급알고리즘1.완전탐색0;
+
+import java.util.Arrays;
+
+/**
+ * Created by masinogns on 2017. 10. 3..
+ */
+public class 순열의집합백준 {
+
+    boolean[] check;
+    long[] f;
+
+    public 순열의집합백준() {
+        check = new boolean[21];
+        f = new long[21];
+        Arrays.fill(f, 1);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
